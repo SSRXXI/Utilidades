191090355f3134d5fcf24f98c24c92ca04a86d67
SSRXXI <sebaraf200@gmail.com>
2021-03-25 17:20:05 -0300

    firmador xml

---
+++ MetodosFirmadores.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Xml;
+
+namespace FirmadorDeXml
+{
+	/// 
+	///	Firma un archivo XML con un certificado
+	/// 
+	public class MetodosFirmadores
+	{
+        public void Firmar()
+        {
+            string archivoFirma = "Firma RM 2020.pfx";
+            X509Certificate2 cert = new X509Certificate2(archivoFirma, "agpsa2020", X509KeyStorageFlags.Exportable);
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.PreserveWhitespace = true;
+            xmlDoc.Load("documento.xml");
+            SignXml(xmlDoc, cert);
+            xmlDoc.Save(@"documentoFirmado.xml");
+        }
+
+        public void SignXml(XmlDocument xmlDoc, X509Certificate2 cert)
+        {
+            if (xmlDoc == null)
+                throw new ArgumentException("xmlDoc");
+            if (cert == null)
+                throw new ArgumentException("Cert");
+
+            var exportedKeyMaterial = cert.PrivateKey.ToXmlString( /* includePrivateParameters = */ true);
+            var key = new RSACryptoServiceProvider(new CspParameters(24 /* PROV_RSA_AES */));
+            key.PersistKeyInCsp = false;
+            key.FromXmlString(exportedKeyMaterial);
+
+            SignedXml signedXml = new SignedXml(xmlDoc);
+            signedXml.SigningKey = key;
+            signedXml.SignedInfo.SignatureMethod = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
+
+            Reference reference = new Reference();
+            reference.AddTransform(new XmlDsigEnvelopedSignatureTransform());
+            reference.AddTransform(new XmlDsigExcC14NTransform());
+            reference.Uri = "";
+            signedXml.AddReference(reference);
+
+            KeyInfo keyInfo = new KeyInfo();
+            keyInfo.AddClause(new KeyInfoX509Data(cert));
+            signedXml.KeyInfo = keyInfo;
+
+            signedXml.ComputeSignature();
+            XmlElement xmlDigitalSignature = signedXml.GetXml();
+            xmlDoc.DocumentElement.AppendChild(xmlDoc.ImportNode(xmlDigitalSignature, true));
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,50 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
-using System.Drawing.Imaging;
-using BitMiracle.Docotic.Pdf;
-using Tesseract;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
 
-namespace Conversion
+namespace FirmadorDeXml
 {
     class Program
     {
         static void Main(string[] args)
         {
-            using (var pdf = new PdfDocument(@"Formulario23.pdf"))
-            {
-                PdfDrawOptions options = PdfDrawOptions.Create();
-                options.BackgroundColor = new PdfRgbColor(255, 255, 255);
-                options.HorizontalResolution = 300;
-                options.VerticalResolution = 300;
+            string archivoFirma = "Firma RM 2020.pfx";
+            X509Certificate2 cert = new X509Certificate2(archivoFirma, "agpsa2020", X509KeyStorageFlags.Exportable);
 
-                // save one page
-                pdf.Pages[0].Save("Formulario.png", options);
-            }
-            Bitmap img = new Bitmap("Formulario.png");
-            TesseractEngine engine = new TesseractEngine("./tessdata","spa",EngineMode.Default);
-            //Page page = engine.Process(img, new Tesseract.Rect(165, 500, 265, 13));
-            Page page = engine.Process(img, new Rect(800,1370,800,45),PageSegMode.Auto);
-           
-            string result = page.GetText();
-
-            int counter = 0;
-            string line;
-
-            // Read the file and display it line by line.  
-            System.IO.StreamReader file =
-                new System.IO.StreamReader(result, Encoding.UTF8);
-            while ((line = file.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
-                counter++;
-            }
-
-            Console.WriteLine(result);
-
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.PreserveWhitespace = true;
+            xmlDoc.Load("documento.xml");
+            MetodosFirmadores metodos = new MetodosFirmadores();
+            metodos.SignXml(xmlDoc, cert);
+            xmlDoc.Save(@"documentoFirmado.xml");
         }
     }
 }
